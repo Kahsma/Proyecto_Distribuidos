1bcfac1b72c05cddbd86d84ac94d28b3cf78add0
Camilo Jose Martinez <cajomapa@gmail.com>
2023-09-22 15:53:12 -0500

    cambios

--- projecto/src/main/java/com/javeriana/Monitor.java
+++ projecto/src/main/java/com/javeriana/Monitor.java
@@ -1,47 +1,34 @@
 package com.javeriana;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 import org.zeromq.ZMQ;
 
 public class Monitor {
     public static void main(String[] args) {
-        String localIpAddress = getLocalIpAddress();
-        if (localIpAddress != null) {
-            ZMQ.Context context = ZMQ.context(1);
-            ZMQ.Socket subscriber = context.socket(ZMQ.SUB);
-            subscriber.connect("tcp://" + localIpAddress + ":5556");
-            subscriber.subscribe("".getBytes());
+        String sensorIpAddress = "192.168.0.11"; // Dirección IP del sensor
+        ZMQ.Context context = ZMQ.context(1);
+        ZMQ.Socket subscriber = context.socket(ZMQ.SUB);
 
-            // Inicia la recepción y procesamiento de mediciones
-            try {
-                while (!Thread.currentThread().isInterrupted()) {
-                    String measurement = subscriber.recvStr();
-                    processMeasurement(measurement);
-                }
-            } finally {
-                subscriber.close();
-                context.term();
+        // Conéctate al sensor (asegúrate de utilizar la dirección y puerto correctos)
+        subscriber.connect("tcp://" + sensorIpAddress + ":5556");
+
+        // Suscríbete a todos los mensajes (en blanco)
+        subscriber.subscribe("".getBytes());
+
+        // Inicia la recepción y procesamiento de mediciones
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                String measurement = subscriber.recvStr();
+                processMeasurement(measurement);
             }
-        } else {
-            System.out.println("No se pudo obtener la dirección IP de la red local.");
+        } finally {
+            subscriber.close();
+            context.term();
         }
     }
 
     // Método para procesar mediciones
     private static void processMeasurement(String measurement) {
         // Implementa la lógica de procesamiento de mediciones aquí
-        System.out.println("Medición recibida: " + measurement);
-    }
-
-    // Método para obtener la dirección IP de la red local
-    private static String getLocalIpAddress() {
-        try {
-            return InetAddress.getLocalHost().getHostAddress();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-            return null;
-        }
+        System.out.println("Medición recibida en el monitor: " + measurement);
     }
 }
--- projecto/src/main/java/com/javeriana/Sensor.java
+++ projecto/src/main/java/com/javeriana/Sensor.java
@@ -2,33 +2,24 @@ package com.javeriana;
 
 import org.zeromq.ZMQ;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.Random;
-
 public class Sensor {
     public static void main(String[] args) {
-        String localIpAddress = getLocalIpAddress();
-        if (localIpAddress != null) {
-            ZMQ.Context context = ZMQ.context(1);
-            ZMQ.Socket publisher = context.socket(ZMQ.PUB);
-            publisher.bind("tcp://" + localIpAddress + ":5556");
+        ZMQ.Context context = ZMQ.context(1);
+        ZMQ.Socket publisher = context.socket(ZMQ.PUB);
+        publisher.bind("tcp://*:5556"); // Escuchar en todas las interfaces de red
 
-            // Simular mediciones y publicarlas
-            try {
-                while (!Thread.currentThread().isInterrupted()) {
-                    double measurement = generateMeasurement();
-                    publisher.send(String.valueOf(measurement), 0);
-                    Thread.sleep(1000); // Simular un intervalo de tiempo
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                publisher.close();
-                context.term();
+        // Simular mediciones y publicarlas
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                double measurement = generateMeasurement();
+                publisher.send(String.valueOf(measurement), 0);
+                Thread.sleep(1000); // Simular un intervalo de tiempo
             }
-        } else {
-            System.out.println("No se pudo obtener la dirección IP de la red local.");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            publisher.close();
+            context.term();
         }
     }
 
@@ -36,15 +27,5 @@ public class Sensor {
     private static double generateMeasurement() {
         // Implementa la lógica de generación de mediciones aquí
         return 68 + Math.random() * 21; // Ejemplo: rango de temperatura
-    }
-
-    // Método para obtener la dirección IP de la red local
-    private static String getLocalIpAddress() {
-        try {
-            return InetAddress.getLocalHost().getHostAddress();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-            return null;
-        }
     }
 }
