bf8f80142b0d638306791f1238a0d81d50bb20c4
Camilo Jose Martinez <cajomapa@gmail.com>
2023-10-25 18:44:13 -0500

    Argumentos

--- projecto/src/main/java/com/javeriana/Monitor.java
+++ projecto/src/main/java/com/javeriana/Monitor.java
@@ -1,27 +1,44 @@
 package com.javeriana;
 
+import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 import org.zeromq.SocketType;
 import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
 import org.zeromq.ZMQ.Socket;
 
 public class Monitor {
+
     @Option(name = "-t", aliases = "--monitorType", required = true, usage = "Monitor type (oxygeno, ph, or temperatura)")
     private SensorType monitorType;
 
-    public static void main(String[] args) throws InterruptedException {
-        try (ZContext context = new ZContext()) {
-            Socket subscriber = context.createSocket(SocketType.SUB);
-            subscriber.connect("tcp://localhost:5560");
+    public static void main(String[] args) {
+        Monitor monitor = new Monitor();
+        CmdLineParser parser = new CmdLineParser(monitor);
 
-            String monitorType = "oxygen#";
-            subscriber.subscribe(monitorType.getBytes());
-            System.out.println("Monitor1 is monitoring " + monitorType);
+        try {
+            // Parse the command-line arguments
+            parser.parseArgument(args);
 
-            while (true) {
-                String message = subscriber.recvStr(0);
-                System.out.println("Monitor1 received: " + message);
+            // Use the parsed monitorType
+            System.out.println("Monitor Type: " + monitor.monitorType);
+
+            try (ZContext context = new ZContext()) {
+                Socket subscriber = context.createSocket(SocketType.SUB);
+                subscriber.connect("tcp://localhost:5560");
+
+                String monitorTypeString = monitor.monitorType.toString() + "#";
+                subscriber.subscribe(monitorTypeString.getBytes());
+                System.out.println("Monitor is monitoring " + monitor.monitorType);
+
+                while (true) {
+                    String message = subscriber.recvStr(0);
+                    System.out.println("Monitor received: " + message);
+                }
             }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            parser.printUsage(System.err);
         }
     }
 }
